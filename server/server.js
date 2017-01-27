9f0885eaf36768a2d4aaa1929d15d622d502eb84
Azeez Olaniran <azeez.olaniran@andela.com>
2017-01-27 17:19:37 +0100

    chore(setup server task runners): fix code styling issues

--- server/server.js
+++ server/server.js
@@ -1,11 +1,1 @@
-import express from 'express';
 
-const app = express();
-console.log('Set up express');
-
-app.get('/', (req, res) => {
-  res.send('Welcome to greatness again and again');
-});
-app.listen(3890, () => {
-  console.log('App Listening on port 3899');
-});
