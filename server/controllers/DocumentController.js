0f32284ed37a3fbe9b1a477268f18a4020edafd0
Azeez Olaniran <azeez.olaniran@andela.com>
2017-02-11 07:47:09 +0100

    feature(implement api endpoints): update documentrelated endpoints to meet tests requirements

--- server/controllers/DocumentController.js
+++ server/controllers/DocumentController.js
@@ -19,13 +19,16 @@ class DocumentController {
       const document = {
         title: request.body.title,
         content: request.body.content,
-        ownerId: request.decoded.userId
+        ownerId: request.decoded.userId,
+        ownerRoleId: request.decoded.roleId,
+        access: request.body.access || 'public' // default access
       };
       documentDb.create(document)
       .then((createdDocument) => {
         response.status(201).json({
           success: true,
-          message: `${createdDocument.title} Successfully Created`
+          message: `${createdDocument.dataValues.title} Successfully Created`,
+          document: createdDocument.dataValues
         });
       })
       .catch((error) => {
@@ -75,22 +78,59 @@ class DocumentController {
 
   /**
    * Controller method fetch a specific Document
+   * Only an admin can fetch all documents regardless of their access type
+   * Non-Admin users can only fetch their private documents and public documents
    * @param{Object} request - Request Object
    * @param{Object} response - Response Object
    * @return{Void} - returns void
    */
   static fetchDocument(request, response){
-    const id = request.params.id;
-    const ownerId = request.decoded.id;
+    const documentId = request.params.id;
+    const requesterId = request.decoded.userId;
+    const requesterRoleId = request.decoded.roleId;
     documentDb.findOne({
       where: {
-        id,
-        ownerId
-      }
+        id: documentId
+      },
+      include: [{
+        model: database.User,
+        attributes: ['roleId']
+      }]
     })
-    .then((document) => {
+    .then((result) => {
+      const document = result ? result.dataValues : null;
       if(document) {
-        response.status(200).json(document);
+        // lets chceck required access
+        // For an Admin, return documents without checking required access
+        if (requesterRoleId === 1) {
+          response.status(200).json({
+            success: true,
+            message: 'Document Found',
+            document
+          });
+          // for other users, ensure they have appropriate access rights
+        } else if (
+          (document.access === 'public'
+          || requesterRoleId === document.User.dataValues.roleId) 
+          && document.access !== 'private') {
+          response.status(200).json({
+            success: true,
+            message: 'Document Found',
+            document
+          });
+        } else if (document.access === 'private'
+          && document.ownerId === requesterId) {
+          response.status(200).json({
+            success: true,
+            message: 'Document Found',
+            document
+          });
+        } else {
+          response.status(401).json({
+            success: false,
+            message: 'Appropriate access is required to view this document'
+          });
+        }
       } else {
         response.status(400).json({
           success: false,
@@ -113,15 +153,53 @@ class DocumentController {
    * @return{Void} - returns void
    */
   static fetchDocuments(request, response){
-    documentDb.findAll({
-      where: {
-        ownerId: request.decoded.userId
-      },
-      attributes: ['id', 'ownerId', 'title', 'content']
-    })
-    .then((documents) => {
-      if (documents) {
-        response.status(200).json(documents);
+    const search = request.query.search;
+    const limit = request.query.limit;
+    const requesterRoleId = request.decoded.roleId;
+    const requesterId = request.decoded.userId;
+    const queryBuilder = {
+      attributes: ['id', 'ownerId', 'access', 'title', 'content', 'createdAt'],
+      include: [{
+        model: database.User,
+        attributes: ['roleId']
+      }]
+    };
+    queryBuilder.order = '"createdAt" DESC';
+    if(limit) {
+      queryBuilder.limit = limit;
+    }
+    if(search) {
+      queryBuilder.where = {
+        $or: [{title: 
+          {$like: `%${search}%`}
+        }, {content: 
+          {$like: `%${search}%`}
+        }]
+      };
+    }
+    documentDb.findAll(queryBuilder)
+    .then((results) => {
+      if (results) {
+        const actualDocuments = [];
+        results.forEach((document) => {
+          if (requesterRoleId === 1) {
+            actualDocuments.push(document.dataValues);
+          // for other users, ensure they have appropriate access rights
+          } else if (
+            (document.access === 'public'
+            || requesterRoleId === document.User.dataValues.roleId) 
+            && document.access !== 'private') {
+            actualDocuments.push(document.dataValues);
+          } else if (document.access === 'private'
+            && document.ownerId === requesterId) {
+            actualDocuments.push(document.dataValues);
+          }
+        });
+        response.status(200).json({
+          success: true,
+          message: 'Documents Fetched',
+          documents: actualDocuments
+        });
       } else {
         response.status(400).json({
           success: false,
