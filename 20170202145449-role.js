4fc31d4c3f23f701ff6e8e19523cc42059fbfacc
Azeez Olaniran <azeez.olaniran@andela.com>
2017-02-15 23:35:56 +0100

    feature(implement api endpoints): update sequelize config file for production database

---
+++ 20170202145449-role.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Roles', [
+      {
+        title: 'admin',
+        id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'regular',
+        id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Roles', null, {});
+  }
+};
